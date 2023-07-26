2ff5fadc2e6ddefab12057a5c57fc7009cdd7943
choiruladamm <choiruladamm@gmail.com>
2023-07-26 20:10:49 +0700

    Setup tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import "tailwindcss/tailwind.css";
+import '@/styles/main.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
