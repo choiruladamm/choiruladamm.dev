8e3b02f876d8418b021b740085bfb3e2ad2325bd
choiruladamm <choiruladamm@gmail.com>
2023-07-29 18:30:40 +0700

    Add Project Heading

--- components/projects/Heading.tsx
+++ components/projects/Heading.tsx
@@ -2,8 +2,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 
-import React from "react";
-
 export function Heading({ tag }: { tag?: string }) {
   return (
     <div
