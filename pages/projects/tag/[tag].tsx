2e69e534f47513f6b35ae5229602c69dc80a2031
choiruladamm <choiruladamm@gmail.com>
2023-07-31 10:46:13 +0700

    Add Filtered by Tag

--- pages/projects/tag/[tag].tsx
+++ pages/projects/tag/[tag].tsx
@@ -1,15 +1,17 @@
-import projects from "@/data/content/projects";
-import { kebabCase } from "@/utils/utils";
+import { Heading, Projects } from "@/components/projects";
+import Page from "@/components/utility/Page";
+import projects, { allKebabTags, allTags } from "@/data/content/projects";
+import { kebabArray, kebabCase } from "@/utils/utils";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Link from "next/link";
 
-export const getStaticPage: GetStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const allTags = [];
   projects.forEach((project) =>
     project.tags.forEach((tag) => {
       allTags.push(tag);
     })
   );
-
   const uniqueAllTags = [...new Set(allTags)];
   const allTagsPaths = uniqueAllTags.map((path) => ({
     params: { tag: `${kebabCase(path)}` },
@@ -20,12 +22,46 @@ export const getStaticPage: GetStaticPaths = async () => {
   };
 };
 
-// export const getStaticProps: GetStaticProps = async ({params}: {params: {tag: string}}) => {
-  
-// }
+export const getStaticProps: GetStaticProps = async ({
+  params,
+}: {
+  params: { tag: string };
+}) => {
+  const tag = params.tag;
+  const filteredProjects = projects.filter((project) =>
+    [...kebabArray(project.tags)].includes(tag)
+  );
+  return {
+    props: JSON.parse(
+      JSON.stringify({
+        filteredProjects,
+        tag: tag,
+      })
+    ),
+  };
+};
 
-function TagPage() {
-  return <div>TagPage</div>;
+function TagPage({ filteredProjects, tag }) {
+  const capsTag = allTags[allKebabTags.indexOf(tag)];
+
+  return (
+    <Page
+      currentPage="Projects"
+      meta={{
+        title: `${capsTag} Projects`,
+        desc: `A showcase for all of my ${capsTag} projects`,
+      }}
+    >
+      <Heading tag={capsTag} />
+      <Projects overwriteProjects={filteredProjects} />
+      
+      <Link href="/projects">
+        <div className="w-full max-w-sm px-8 py-3 mx-auto mt-8 text-center transition-colors border rounded-full cursor-pointer md:max-w-2xl border-fun-pink whitespace-nowrap text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white">
+          View All
+        </div>
+      </Link>
+    </Page>
+  );
 }
 
 export default TagPage;
