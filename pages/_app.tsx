8de9dd5c1e1ffe11e56e8f115889625dee7b9e31
choiruladamm <choiruladamm@gmail.com>
2023-10-01 11:26:02 +0700

    feat: add progress-bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "tailwindcss/tailwind.css";
 import '@/styles/main.css'
+import NextTopLoader from "nextjs-toploader";
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextTopLoader color="#00c7ff" showSpinner={false} />
+      <Component {...pageProps} />
+    </>
+  );
 }
