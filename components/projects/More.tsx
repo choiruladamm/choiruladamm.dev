f9befacd297616ac0196b17231bf9f3c55351e61
choiruladamm <choiruladamm@gmail.com>
2023-10-09 12:37:57 +0700

    refactor: change a tag to link & fix bug target _blank

--- components/projects/More.tsx
+++ components/projects/More.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
+
 export function More() {
   return (
-    <p className="py-16 text-sm  text-fun-gray">
+    <p className="py-16 text-sm text-fun-gray">
       Chek my{" "}
-      <a
+      <Link
         href="https://github.com/choiruladamm"
+        target="_blank"
         className="italic underline text-fun-pink"
       >
         Github
-      </a>{" "}
+      </Link>{" "}
       for more project!😁
     </p>
   );
